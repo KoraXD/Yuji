62fd1fd90b9a3af3f1529e5645de8493749dee13
Kora <115209660+KoraXD@users.noreply.github.com>
2024-01-28 18:36:43 +0530

    .

--- Exon/modules/error_handling.py
+++ Exon/modules/error_handling.py
@@ -108,7 +108,7 @@ def error_callback(update: Update, context: CallbackContext):
         url = f"https://spaceb.in/{response['payload']['id']}"
         context.bot.send_message(
             ERROR_LOGS,
-            text=f"#{context.error.identifier}\n<b>Error::"
+            text=f"#{context.error.identifier}\n<b>Error:"
             f"</b>\n<code>{e}</code>",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("ʟᴏɢs", url=url)]],
