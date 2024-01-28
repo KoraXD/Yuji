b5d39b593327a0a5fb2d93325663ef0f0674c61b
Kora <115209660+KoraXD@users.noreply.github.com>
2024-01-28 18:05:34 +0530

    .

--- Exon/modules/helper_funcs/misc.py
+++ Exon/modules/helper_funcs/misc.py
@@ -98,7 +98,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             )
         )
 
-    COLUMN_SIZE = 6
+    COLUMN_SIZE = 10
 
     max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
     modulo_page = page_n % max_num_pages
